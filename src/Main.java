24816c3370aacfbff3473ac55adf56c36b6b72a1
Leyla1987 <glenmorangie@list.ru>
2022-08-22 20:13:03 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 21;
+    if (age == 18) {
+        System.out.println("С окончанием школы!!))");
+    } else if (age == 21) {
+        System.out.println("Теперь можешь бухать");
+    } else if (age == 7){
+        System.out.println("Иди в школу, дружок!!");
+    } else {
+        System.out.println("Новостей для тебя пока нет((");
+    }
+     }
+    }
